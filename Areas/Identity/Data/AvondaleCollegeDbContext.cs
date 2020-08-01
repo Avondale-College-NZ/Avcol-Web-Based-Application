05a8e02df588f08779247525b4228ec4df470b30
Rohan Patel <ac98853@avcol.school.nz>
2020-08-02 11:08:12 +1200

    Add project files.

---
+++ Areas/Identity/Data/AvondaleCollegeDbContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Avondale_College.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Avondale_College.Data
+{
+    public class AvondaleCollegeDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public AvondaleCollegeDbContext(DbContextOptions<AvondaleCollegeDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Avondale_College.Areas.Identity.Data;
+using Avondale_College.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Avondale_College.Areas.Identity.IdentityHostingStartup))]
+namespace Avondale_College.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<AvondaleCollegeDbContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("AvondaleCollegeDbContextConnection")));
+
+                services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<AvondaleCollegeDbContext>();
+            });
+        }
+    }
+}

c30a67d9c32a8036599557e4768bc242e929b5c8
Rohan Patel <ac98853@avcol.school.nz>
2020-08-02 15:46:10 +1200

    I have made some small changes on the login page and register page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,9 +6,11 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Avondale_College.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Avondale_College.Controllers
 {
+    
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
